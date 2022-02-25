87333b97967f223386f6614e60fd208276152baa
WuChengpei_Sky <w2563070231@outlook.com>
2022-02-25 10:09:36 +0800

    New Code

--- pack_img.py
+++ pack_img.py
@@ -1,16 +1,14 @@
 import openpyxl
 import os
 import urllib.request
+import urllib.error
 import datetime
 
 file_name = '收集结果.xlsx'
 data_dic = {}
 date = datetime.date.today()
-main_save_path = '.\\' + str(date.year) + str(date.month) + str(date.day) + '信息xs2101'
+main_save_path = '.\\{}{:0>2d}{:0>2d}信息xs2101'.format(date.year, date.month, date.day)
 save_path = [main_save_path + '\\健康码', main_save_path + '\\行程码', main_save_path + '\\同行密接人员自查']
-head = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"}
 
 
 def save_data():
@@ -27,17 +25,24 @@ def save_data():
 
 
 def download_img():
-    for key in data_dic:
+    head = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"}
+    for name in data_dic:
         index = 0
-        for url in data_dic.get(key):
+        for url in data_dic.get(name):
             request = urllib.request.Request(url, headers=head)
             try:
                 response = urllib.request.urlopen(request)
-                img_name = key + '.jpeg'
+                img_name = name + '.jpeg'
                 if response.getcode() == 200:
                     with open(save_path[index] + '\\' + img_name, 'wb') as file:
                         file.write(response.read())
-            except:
+            except urllib.error.URLError as e:
+                if hasattr(e, 'code'):
+                    print(e.code)
+                if hasattr(e, 'reason'):
+                    print(e.reason)
                 print('failed!')
             index += 1
     print('搞定了')
@@ -46,22 +51,25 @@ def download_img():
 def show_profile():
     print('这是一个临时用于打包打卡数据的python代码')
     print('----------------------------------')
-    print('使用前请确保与 .py/.exe 文件同一目录下\n有且仅有一个 收集数据.xlsx 文件')
+    print('使用前请确保与 .py/.exe 文件同一目录下\n有且仅有一个 收集数据 .xlsx 文件')
     print('一定要使用腾讯文档导出的 .xlsx 文件')
     print('请严格按照QQ群中发送的收集表顺序填写')
     print('遇到bug，及时联系开发人员，比如对面宿舍')
     print('-----------------------------------')
-    print('详细阅读后,键入 y 开始打包')
+    print('详细阅读后,键入y开始打包,n退出,其他无效')
 
 
 if __name__ == '__main__':
-    show_profile()
-    select = str(input())
-    if select == 'y':
-        if not os.path.exists(main_save_path):
-            os.mkdir(main_save_path)
-            os.mkdir(save_path[0])
-            os.mkdir(save_path[1])
-            os.mkdir(save_path[2])
-        save_data()
-        download_img()
+    is_exit = False
+    while not is_exit:
+        show_profile()
+        select = str(input())
+        if select == 'y':
+            if not os.path.exists(main_save_path):
+                os.mkdir(main_save_path)
+                for path in save_path:
+                    os.mkdir(path)
+            save_data()
+            download_img()
+        elif select == 'n':
+            is_exit = True
