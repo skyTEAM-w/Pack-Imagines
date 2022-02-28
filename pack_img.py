b79ed2b90269d8f72c47978254211d46f19fa3ee
WuChengpei_Sky <w2563070231@outlook.com>
2022-02-28 17:01:27 +0800

    记忆化搜索

--- pack_img.py
+++ pack_img.py
@@ -8,8 +8,10 @@ import urllib.request
 import openpyxl
 from paddleocr import PaddleOCR
 
-data_dic = {}
 date = datetime.date.today()
+read_path = '.\\list.txt'
+check_list_path = '.\\check_list.txt'
+out_put_path = '.\\output.txt'
 main_save_path = '.\\{}{:0>2d}{:0>2d}信息xs2101'.format(date.year, date.month, date.day)
 save_path = [main_save_path + '\\健康码', main_save_path + '\\行程码', main_save_path + '\\同行密接人员自查']
 check_path = '.\\temp'
@@ -19,13 +21,17 @@ Day = r'{}-{:0>2d}-{:0>2d}.*'.format(date.year, date.month, date.day)
 Day2 = r'.*{}\.{:0>2d}\.{:0>2d}.*'.format(date.year, date.month, date.day)
 file_name = '{}月{:0>2d}日信息xs2101“两码一查询”（收集结果）.xlsx'.format(date.month, date.day)
 
+data_dic = {}
+student_list = []
+check_list = {'存在问题': {}, '未填报': []}
+
 
 def save_data():
     main_book = openpyxl.load_workbook(file_name)
     main_sheet = main_book.active
 
     for i in range(2, main_sheet.max_row):
-        with main_sheet.cell(i, 1).value is not None:
+        if main_sheet.cell(i, 1).value is not None:
             name = main_sheet.cell(i, 3).value
             url = []
 
@@ -37,7 +43,7 @@ def save_data():
             data_dic.update({name: [url,
                                     [str(url[i])[23:66:1].replace('/', '').replace('.', '')
                                         .replace('?', '') + '.jpeg'
-                                        for i in range(3)]]})
+                                     for i in range(3)]]})
             print(i - 1, end=' ')
 
     print()
@@ -55,12 +61,14 @@ def download_img():
                 response = urllib.request.urlopen(request)
                 img_name = name + '.jpeg'
 
-                if response.getcode() == 200 and not os.path.exists(save_path[index] + '\\' + img_name):
+                if response.getcode() == 200 and (not os.path.exists(save_path[index] + '\\' + img_name) or name in
+                                                  check_list['存在问题']):
                     with open(save_path[index] + '\\' + img_name, 'wb') as file:
                         file.write(response.read())
 
-                shutil.copyfile(save_path[index] + '\\' + img_name,
-                                temp_path[index] + '\\' + data_dic.get(name)[1][index])
+                if not os.path.exists(save_path[index] + '\\' + img_name) or name in check_list['存在问题']:
+                    shutil.copyfile(save_path[index] + '\\' + img_name,
+                                    temp_path[index] + '\\' + data_dic.get(name)[1][index])
 
             except urllib.error.URLError as e:
                 if hasattr(e, 'code'):
@@ -72,6 +80,10 @@ def download_img():
             index += 1
 
         print(name, end=' ')
+
+    for name in student_list:
+        if name not in data_dic.keys():
+            check_list['未填报'].append(name)
 
     print()
 
@@ -89,7 +101,7 @@ def show_profile():
     print('详细阅读后,键入y开始打包,n退出,其他无效')
 
 
-def check():
+def check(file2):
     print('是否对生成文件进行身份证检查？\ny:是\t其他:否')
     is_stop = False
     problem = {}
@@ -97,7 +109,9 @@ def check():
     while not is_stop:
         choice = str(input())
         if choice == 'y':
-            for name_t in data_dic:
+            for name_t in \
+                    (data_dic if len(check_list['存在问题']) == 0
+                        else check_list['存在问题'][0:len(check_list['存在问题']) - 1]):
                 error = []
                 index = 0
 
@@ -115,7 +129,7 @@ def check():
                     if index == 0:
                         if not flag1 or not flag2:
                             error.append(save_path[index].replace(main_save_path + '\\', '') + ':' +
-                                         ('' if flag1 else '身份证显示不全') + '\t' +
+                                         ('' if flag1 else '身份证显示不全') + ' ' +
                                          ('' if flag2 else '非当日截图'))
                     elif index == 2:
                         if not flag1:
@@ -138,8 +152,18 @@ def check():
 
             is_stop = True
             print('------------------------------------------------------')
-            print(problem)
+            file = open(check_list_path, 'w')
+            for name in problem:
+                file.write(name + '\n')
+                file2.write(name + ':')
+                print(name + ':', end='')
+                for wrong in problem[name]:
+                    file2.write(wrong + ' ')
+                    print(wrong, end=' ')
+                print()
+                file2.write('\n')
             print('以上同学可能存在问题，请查看！')
+            check_list['存在问题'] = problem
         else:
             is_stop = True
 
@@ -151,6 +175,19 @@ if __name__ == '__main__':
             show_profile()
             select = str(input())
             if select == 'y':
+
+                # 初始化
+                out_file = open(out_put_path, 'w')
+                out_file.write(str(datetime.datetime.now()) + '\n')
+                out_file.write('--------------------\n')
+
+                if os.path.exists(read_path):
+                    in_file = open(read_path, 'r')
+                    student_list = in_file.read().split('\n')
+                if os.path.exists(check_list_path):
+                    file = open(check_list_path, 'r')
+                    check_list['存在问题'] = file.read().split('\n')
+
                 if not os.path.exists(main_save_path):
                     os.mkdir(main_save_path)
                     for path in save_path:
@@ -159,9 +196,15 @@ if __name__ == '__main__':
                     os.mkdir(check_path)
                     for temp in temp_path:
                         os.mkdir(temp)
+                # 实际操作区
                 save_data()
                 download_img()
-                check()
+                check(out_file)
+                out_file.write('--------------------\n')
+                for name in check_list['未填报']:
+                    if len(check_list['未填报']) > 0:
+                        out_file.write(name + ':' + '未填报' + '\n')
+                print(check_list)
                 is_exit = True
             elif select == 'n':
                 is_exit = True
